b280cb7d0ef61c191cb55a2fa76d94f3557e4ed2
Moee21 <mohamed_sheik@outlook.com>
2022-07-18 17:48:34 +0000

    print instruction to the user

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+TOTAL_STICKS = 21          # Total number of match sticks
+
+print("This program is a game which you will play against the computer")
+print("In this game there are 21 sticks and")
+print("you have to pick up 1-4 number of sticks each turn ")
+print("The way you win is by not taking the last stick \n")
+
+print("Total Sticks:", TOTAL_STICKS, "\n")
+
