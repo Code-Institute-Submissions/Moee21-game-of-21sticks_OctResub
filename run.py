e0e64c8a849c38768fd55576f4295cac348affe0
Moee21 <mohamed_sheik@outlook.com>
2022-07-18 20:33:23 +0000

    Add reference

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
-TOTAL_STICKS = 21          # Total number of match sticks
+"""
+got idea and inspiration from:
+https://www.youtube.com/watch?v=ReQz7vwDjPs&ab_channel=NickyDylewski
+https://www.youtube.com/watch?v=SdrSEq_dsBU&t=326s&ab_channel=3ATeam
+"""
+
+STICKS = 21          # Total number of match sticks
 
 print("This program is a game which you will play against the computer")
 print("In this game there are 21 sticks and")
 print("you have to pick up 1-4 number of sticks each turn ")
 print("The way you win is by not taking the last stick \n")
 
-print("Total Sticks:", TOTAL_STICKS, "\n")
+print("Total Sticks:", STICKS, "\n")
 
